2daf65e0507633adf04f633bbe7a932853030409
therecluse26 <therecluse26@protonmail.com>
2022-12-06 23:04:47 -0500

    Got email and github oauth working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "../../../lib/prismadb"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email"
+
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
@@ -11,8 +13,20 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: 'NextAuth <noreply@example.com>',
+        }),
         // Add more providers here
     ],
+
 }
 
 export default NextAuth(authOptions)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
+import LoginBtn from '../components/login-btn'
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+
+        <LoginBtn />
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
