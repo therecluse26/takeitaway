455630c0083437dd84e65ec79b15fec538dca0f5
therecluse26 <innatesignal@protonmail.com>
2022-12-06 21:12:38 -0500

    Test update

--- pages/index.js
+++ pages/index.js
@@ -37,15 +37,7 @@ export default function Home() {
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+
         </div>
       </main>
 
