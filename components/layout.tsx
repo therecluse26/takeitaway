ff57b110ae67bcf51435ee437665021c99a6c523
therecluse26 <therecluse26@protonmail.com>
2023-01-08 11:30:29 -0500

    Added scrolltotop

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,6 +4,9 @@ import { defaultNavBarLinks, buildNavbarLinks } from "../data/navbar-links";
 import { ReactElement, useEffect, useState } from "react";
 import React from "react";
 import dynamic from "next/dynamic";
+import { Affix, Transition, Button } from "@mantine/core";
+import { IconArrowUp } from "@tabler/icons";
+import { useWindowScroll } from "@mantine/hooks";
 
 const AppShell = dynamic(() =>
   import("@mantine/core").then((mod) => mod.AppShell)
@@ -13,6 +16,7 @@ const Layout = ({ children }: { children: ReactElement<any> }) => {
   const { data: session, status } = useSession();
   const [links, setLinks] = useState(defaultNavBarLinks);
   const [linksHaveBeenBuilt, setLinksHaveBeenBuilt] = useState(false);
+  const [scroll, scrollTo] = useWindowScroll();
 
   useEffect(() => {
     if (!linksHaveBeenBuilt) {
@@ -23,7 +27,24 @@ const Layout = ({ children }: { children: ReactElement<any> }) => {
 
   return (
     <AppShell padding={0} navbar={<Navbar links={links} />}>
-      <div style={{ marginTop: "120px" }}>{children}</div>
+      <div style={{ marginTop: "120px" }}>
+        <>
+          {children}
+          <Affix position={{ bottom: 20, right: 20 }}>
+            <Transition transition="slide-up" mounted={scroll.y > 0}>
+              {(transitionStyles) => (
+                <Button
+                  variant="light"
+                  style={transitionStyles}
+                  onClick={() => scrollTo({ y: 0 })}
+                >
+                  <IconArrowUp size={16} />
+                </Button>
+              )}
+            </Transition>
+          </Affix>
+        </>
+      </div>
     </AppShell>
   );
 };
