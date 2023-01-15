0da6143b7b640ab750cb3fa4c2343d001555d141
therecluse26 <therecluse26@protonmail.com>
2023-01-14 23:53:21 -0500

    Removing app dir until it's out of beta

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 128, 256, 384],
     minimumCacheTTL: 86400,
   },
+  experimental: {
+    appDir: false,
+  },
+  ...withBundleAnalyzer({})
 }
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
